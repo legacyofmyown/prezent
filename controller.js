0e52b73e81693e08d9bf11e0bc1fde30a2db3200
Lindsay Macvean <lindsaymacvean@gmail.com>
2013-10-25 13:37:59 -0700

    added mongo and heroku

--- controller.js
+++ controller.js
@@ -1,9 +1,12 @@
-var express = require('express');
+var express = require("express");
 var app = express();
+app.use(express.logger());
 
-app.get('/hello.txt', function(req,res){
-    var body = 'Hello World';
-    res.setHeader('Content-Type','text/plain');
-    res.setHeader('Content-Length', body.length);
-    res.end(body);
+app.get('/', function(request, response) {
+  response.send('Hello World!');
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
 });
